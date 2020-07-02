8a9cdd1cb40cdd3aeb83d38e470b8c7715fca48b
Lennart Keller <lennart.keller@googlemail.com>
2020-07-02 15:15:59 +0200

    decrease sequence max length

--- embeddings/bert_summaries.py
+++ embeddings/bert_summaries.py
@@ -30,8 +30,8 @@ if __name__ == '__main__':
 
         # heat and tail method (https://arxiv.org/pdf/1905.05583.pdf)
         token = row.Plot.split()
-        if len(token) > 512:
-            text = " ".join(token[:512])
+        if len(token) > 400:
+            text = " ".join(token[:400])
         else:
             text = " ".join(token)
 
