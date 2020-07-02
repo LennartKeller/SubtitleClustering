7e61c5b5ffa4339b00908362890b859f04f26993
Lennart Keller <lennart.keller@googlemail.com>
2020-07-02 14:53:57 +0200

    update notebooks and add script for bert embeddings of summaries

---
+++ embeddings/bert_summaries.py
@@ -0,0 +1,77 @@
+import re
+
+import nltk
+import numpy as np
+import pandas as pd
+from flair.data import Sentence
+from flair.embeddings import TransformerDocumentEmbeddings
+from torch.cuda import is_available
+from tqdm import tqdm
+import re
+
+if __name__ == '__main__':
+
+    print("Checking if CUDA is available...")
+    print("CUDA is enabled" if is_available() else "CUDA is not enabled")
+    print("Reading dataset...")
+    df = pd.read_csv('../dataset/movies_complete.csv')
+    df.drop_duplicates(subset=['IMDB_ID'], inplace=True)
+    df.dropna(subset=['filename'], inplace=True)
+    df.dropna(subset=['Plot'], inplace=True)
+
+    print("Filtering noise from text data...")
+    remove_subcentral_annotation = re.compile(r'übersetzt von.+$', flags=re.I)
+    remove_season_episode_annotation = re.compile(r's\d+?e\d+?', flags=re.I)
+    remove_remove_season_episode_annotation_german = re.compile(r'staffel.+?episode', flags=re.I)
+    remove_remove_season_episode_annotation_german_2 = re.compile(r'staffel.+?folge', flags=re.I)
+    remove_subcentral_note = re.compile(r'subcentral.+ präsentiert', flags=re.I)
+    remove_subtitle_statement = re.compile(r'Untertitel von.+$', flags=re.I)
+    remove_netflix_original_statement = re.compile(r'EINE NETFLIX ORIGINAL SERIE', flags=re.I)
+    remove_tvuser = re.compile(r'tv4user.+?präsentiert', flags=re.I)
+    remove_tvuser_subcentral = re.compile(r'subcentral.+?tv4user', flags=re.I)
+    remove_subcentral_url = re.compile(r'subcentral\.de', flags=re.I)
+    remove_tv4user_url = re.compile(r'tv4user\.de', flags=re.I)
+    remove_sub_statement = re.compile(r'subbed\w+?by', flags=re.I)
+    remove_tiger_statement = re.compile(r'übersetzung filmtiger', flags=re.I)
+
+    for regex in (remove_subcentral_annotation,
+                  remove_season_episode_annotation,
+                  remove_remove_season_episode_annotation_german,
+                  remove_remove_season_episode_annotation_german_2,
+                  remove_subcentral_note,
+                  remove_subtitle_statement,
+                  remove_netflix_original_statement,
+                  remove_tvuser,
+                  remove_tvuser_subcentral,
+                  remove_subcentral_url,
+                  remove_tv4user_url,
+                  remove_sub_statement,
+                  remove_tiger_statement):
+        df.text = df.text.str.replace(regex, '')
+
+    doc_embeddings = TransformerDocumentEmbeddings('bert-base-german-cased')
+
+    embeddings = []
+
+    print("Start computing embeddings..")
+    for index, row in tqdm(df.iterrows()):
+        if not row.Plot:
+            continue
+
+        # heat and tail method (https://arxiv.org/pdf/1905.05583.pdf)
+        token = re.findall(r'\w+', row.Plot)
+        if len(token) > 512:
+            head = token[:128]
+            tail = token[382:]
+            text = " ".join(token[:512])
+        else:
+            text = " ".join(token)
+
+        doc = Sentence(text)
+
+        doc_embeddings.embed(doc)
+        sequence_embedding = doc.embedding.cpu().detach().numpy()
+
+        with open('embeddings_bert_summaries.txt', 'a') as f:
+            f.write(f"{row.filename} {' '.join(map(str, sequence_embedding))}\n")
+
--- misc/get_metadata_openIMDB.py
+++ misc/get_metadata_openIMDB.py
@@ -1,11 +1,19 @@
 import requests
 
+# 
+class RequestException(Exception):
+    pass
+
 
 def get_metadata(imdb_id: str, api_key: str, plot_type: str = "full") -> dict:
     response = requests.get(f"http://www.omdbapi.com/?apikey={api_key}&i={imdb_id}&plot={plot_type}")
     if response.status_code != 200:
-        raise Exception(f"Last reponse status code was {response.status_code}\n{response.content}")
-    return response.content
+        raise RequestException(f"Last reponse status code was {response.status_code}\n{response.content}")
+    try:
+        data = response.json()
+        return data
+    except Exception:
+        return
 
 
 if __name__ == '__main__':
@@ -14,29 +22,30 @@ if __name__ == '__main__':
     from pathlib import Path
     from tqdm import tqdm
 
-    api_key = input("Enter APi-Key")
+    api_key = input("Enter APi-Key: ")
     movies = pd.read_csv('../dataset/movies.csv')
-    if Path('../dataset/openIMDB.csv').exists():
-        openimdb = pd.read_csv('../dataset/openIMDB.csv')
-        prexeisting_data = True
-    else:
+    if not Path('../dataset/openIMDB.csv').exists():
         openimdb = pd.DataFrame()
-        prexeisting_data = False
+        openimdb['imdbID'] = []
+        openimdb.to_csv('../dataset/openIMDB.csv')
 
-    data = []
-    for imdb_id in tqdm(movies['IMDB_ID']):
-        try:
-            if prexeisting_data:
-                if imdb_id not in openimdb['imdbID']:
-                    data.append(get_metadata(imdb_id, api_key))
-            else:
-                data.append(get_metadata(imdb_id, api_key))
-        except Exception as e:
-            print(e)
+    openimdb = pd.read_csv('../dataset/openIMDB.csv')
+    entries = []
+    pbar = tqdm(movies['IMDB_ID'])
+    for imdb_id in pbar:
+        if imdb_id in openimdb['imdbID'].to_list():
+            pbar.set_description(f'Already got data for {imdb_id}')
+            continue
+        else:
+            pbar.set_description(f'Load data for {imdb_id}')
+            try:
+                data = get_metadata(imdb_id, api_key)
+                if data:
+                    entries.append(data)
+            except RequestException as e:
+                pbar.set_description(f'Error: {e}')
+                break
 
-    new_df = pd.DataFrame.from_records(data)
-
-    openimdb.append(new_df)
+    new_df = pd.DataFrame.from_records(entries)
+    openimdb = openimdb.append(new_df)
     openimdb.to_csv('../dataset/openIMDB.csv')
-
-
